ce14a083e158b5a894339196cc5728b7037092eb
kanna228 <149002115+kanna228@users.noreply.github.com>
2024-11-20 16:55:21 +0500

    Delete ZD-Aura(web app) directory

--- ZD-Aura(web app)/script_faq.js
+++
@@ -1,6 +0,0 @@
-function hideAndShowAnswer(index) {
-    const answer = document.getElementById('answer' + index);
-    const displayStatus = answer.style.display;
-
-    answer.style.display = displayStatus === 'block' ? 'none' : 'block';
-}
